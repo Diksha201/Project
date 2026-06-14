2d2da6f3c840f1dc1e62bf307fa09b616d4a587e
Diksha Kumari Singh <130738814+Diksha201@users.noreply.github.com>
2026-06-14 13:25:14 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-  mongoose.connect(process.env.MONGO_URI);
-    console.log(` MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(` Error: ${err.message}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
     process.exit(1);
   }
 };
